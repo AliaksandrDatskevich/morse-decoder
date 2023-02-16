02526383ac85fd020de25f2cb338931043098d5b
Aliaksandr Datskevich <datskevich.a@icloud.com>
2023-02-16 23:48:53 +0300

    done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arrBinary = [];
+  let arrMorse = [];
+  let result = '';
+
+  for (let i = 0; i < expr.length; i += 10) {
+    arrBinary.push(expr.slice(i, i + 10));
+  }
+  for (let i = 0; i < arrBinary.length; i++) {  
+    arrMorse[i] = '';
+    for (let j = 0; j < 10; j += 2) {
+      let current = (arrBinary[i][j] + arrBinary[i][j + 1]);
+      if ((current) === '00') {
+        arrMorse[i] += ''
+      }
+      if ((current) === '10') {
+        arrMorse[i] += '.'
+      }
+      if ((current) === '11') {
+        arrMorse[i] += '-'
+      }
+    }
+  }
+
+  for (let i = 0; i < arrMorse.length; i++) {
+    if (arrMorse[i] !== '') {
+      result += MORSE_TABLE[arrMorse[i]];
+    } else {
+      result += ' '
+    }
+  }
+  return result
 }
 
 module.exports = {
